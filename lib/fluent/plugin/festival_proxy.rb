e7658f9f2f8c932c511a6046712854728448d1f1
Kenji Okimoto <okimoto@clear-code.com>
2017-04-27 16:46:18 +0900

    Use `log` method instead of `$log`

--- lib/fluent/plugin/festival_proxy.rb
+++ lib/fluent/plugin/festival_proxy.rb
@@ -10,7 +10,7 @@ module Fluent::Plugin
     end
 
     def start_proxy
-      $log.debug "start festival proxy #{@api_uri}"
+      log.debug "start festival proxy #{@api_uri}"
 
       @uri = URI.parse(@api_uri)
       @https = Net::HTTP.new(@uri.host, @uri.port)
@@ -19,17 +19,17 @@ module Fluent::Plugin
     end
 
     def shutdown_proxy
-      $log.debug "shutdown_proxy #{@session.inspect}"
+      log.debug "shutdown_proxy #{@session.inspect}"
       delete_session
       @https.finish() if @https.active?
     end
 
     def error_handler(response, message)
       if response.code != "200"
-        $log.error :error => message
-        $log.debug "code: #{response.code}"
-        $log.debug "message: #{response.message}"
-        $log.debug "body: #{response.body}"
+        log.error :error => message
+        log.debug "code: #{response.code}"
+        log.debug "message: #{response.message}"
+        log.debug "body: #{response.body}"
         return false
       end
       return true
@@ -90,7 +90,7 @@ module Fluent::Plugin
 
       get_data_req = @uri + Pathname("/festival/eaas/experimentation/#{path}").cleanpath.to_s
       #get_data_req.query = URI.encode_www_form(get_data_params)
-      $log.debug "#{get_data_req}"
+      log.debug "#{get_data_req}"
       # currently time window is automatically updated
       #@from = Time.now.iso8601
       get_data_req
@@ -111,28 +111,28 @@ module Fluent::Plugin
     def get_data
       if !valid_session?
         return nil if create_session.nil?
-        $log.debug "session #{@session} created."
+        log.debug "session #{@session} created."
       end
       data = []
       #require 'pry-byebug'
-      $log.debug "@resources: #{@resources.inspect}"
+      log.debug "@resources: #{@resources.inspect}"
       @resources.each do |resource|
         case resource_type(resource.path)
         when "current_data" then
-          $log.debug "get_data: request #{get_data_request(resource.path)}, #{get_data_header.inspect}"
+          log.debug "get_data: request #{get_data_request(resource.path)}, #{get_data_header.inspect}"
           get_data_res = @https.get(get_data_request(resource.path), get_data_header)
           return nil if !error_handler(get_data_res,"get_data failed.")
-          $log.debug "get_data: #{get_data_res.body}"
+          log.debug "get_data: #{get_data_res.body}"
           data << {
             "resourceName": resource.path,
             "dataValue": JSON.parse(get_data_res.body)["dataValue"]
           }
         when "historical_data" then
-          $log.error "historical_data is not supported yet"
+          log.error "historical_data is not supported yet"
           return nil
         else
-          $log.error "The other resource type is not supported yet"
-          $log.error "resource_type: #{resource_type(resource.path)}"
+          log.error "The other resource type is not supported yet"
+          log.error "resource_type: #{resource_type(resource.path)}"
           return nil
         end
       end
